2dbc09a7c8db84211703c9622ceb79983d4a1d28
Michael Grant <mdgrant116@gmail.com>
2020-08-08 15:10:49 -0400

    Initial Commit

---
+++ SpotifyClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Michael Grant on 8/8/20.
+//  Copyright © 2020 Michael Grant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
